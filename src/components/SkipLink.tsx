d61363e17c319a12c52fea569d2cdf45329af4fb
Rizky Ardiansyah <rizkyardiansyah@disroot.org>
2023-10-12 09:58:56 +0700

    Missing semicolon and newline on `SkipLink` components

--- src/components/SkipLink.tsx
+++ src/components/SkipLink.tsx
@@ -1,3 +1,3 @@
 export default function SkipLink() {
-  return <a className="skip-link" href="#content">Skip to Content</a>
+  return <a className="skip-link" href="#content">Skip to Content</a>;
 }
